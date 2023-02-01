02d45c4a3b3c45237e27f1f0e69bec05e5e7492f
Luc Hermkens <hermkensluc@gmail.com>
2023-02-01 14:49:27 +0100

    rule updates

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,19 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+// TODO: add more rules and test cases
+
 void (async () => {
     const result = await fs.promises.readFile(
         path.resolve(__dirname, './test.ts'),
     );
 
-    console.log(result);
+    // @typescript-eslint/prefer-reduce-type-parameter
+    Object.entries({ foo: 'bar' }).reduce((acc, [key, value]) => {
+        acc[key] = value.length;
+        return acc;
+    }, {} as Record<string, number>);
+
+    // @typescript-eslint/no-confusing-void-expression
+    return void console.log(result);
 });
